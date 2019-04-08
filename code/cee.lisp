722f4bd204214792399ca516d2f8e3eb6d428e30
Christophe Junk <junke.christophe@gmail.com>
2019-04-08 16:14:26 +0200

    Fix compilation

--- code/cee.lisp
+++ code/cee.lisp
@@ -1,5 +1,22 @@
 (defpackage :bricabrac.code.cee
-  (:use :cl :alexandria))
+  (:use :cl :alexandria)
+  (:export #:*c-environment*
+           #:*c-indent*
+           #:*c-right-margin*
+           #:with-c
+           #:with-logical-block
+           #:with-indent
+           #:code-dispatch
+           #:c/block
+           #:c/block*
+           #:c/switch
+           #:c/struct
+           #:c/case
+           #:c/function
+           #:nl
+           #:declarationp
+           #:call-with-multiple-c-contexts
+           #:with-multiple-c-contexts))
 
 (in-package :bricabrac.code.cee)
 
@@ -18,24 +35,34 @@
 ;; job the enclosing scope to dedent and newline.
 ;;
 
-(defvar *c-environment* :definition)
-(defvar *c-indent* 4)
-(defvar *c-right-margin* 80)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *c-environment* :definition)
+  (defvar *c-indent* 4)
+  (defvar *c-right-margin* 80)
+  (defparameter *c-signature-formats*
+    (list (cons :filled "~:[~;static ~]~a ~a(~@<~{~a~^, ~:_~}~:>)")
+          (cons :linear "~:[~;static ~]~a ~a(~@<~{~a~^, ~_~}~:>)")
+          (cons :linear/linear
+                "~:[~;static ~]~a ~_~a(~@<~{~a~^, ~_~}~:>)"))))
 
-(defvar *c-signature-formats*
-  (list (cons :filled "~:[~;static ~]~a ~a(~@<~{~a~^, ~:_~}~:>)")
-        (cons :linear "~:[~;static ~]~a ~a(~@<~{~a~^, ~_~}~:>)")))
+(defmacro with-multiple-c-contexts ((&key &allow-other-keys)
+                                    &body body)
+  `(call-with-multiple-c-contexts (lambda () ,@body)))
+
+(defun call-with-multiple-c-contexts (function)
+  (dolist (*c-environment* '(:declaration :definition))
+    (funcall function)))
 
 (defmacro with-c (&body body)
-  `(let ((*print-right-margin* ,*c-right-margin*))
+  `(let ((*print-right-margin* *c-right-margin*))
      ,@body))
 
 (defmacro with-logical-block (&body body)
   `(pprint-logical-block (*standard-output* ())
      ,@body))
 
-;; (defun nl (&optional (kind :mandatory))
-;;   (pprint-newline kind))
+(defun nl (&optional (kind :mandatory))
+  (pprint-newline kind))
 
 (defmacro with-indent ((&optional (indent '*c-indent*)) &body body)
   (with-gensyms (indent%)
@@ -46,6 +73,15 @@
 
 (defmacro c/block (&body body)
   `(with-logical-block
+     (princ "{")
+     (with-indent ()
+       (nl)
+       ,@body)
+     (nl)
+     (princ "}")))
+
+(defmacro c/block* (&body body)
+  `(progn
      (princ "{")
      (with-indent ()
        (nl)
@@ -81,12 +117,20 @@
 (defun declarationp ()
   (eq *c-environment* :declaration))
 
-(defmacro c/function ((&key
-                         name
-                         (staticp nil)
-                         (format :linear)
-                         (ret "void")
-                         (parameters '("void")))
+(defmacro code-dispatch (&body clauses)
+  (flet ((only (x)
+           (mapcan #'cdr
+                   (remove x clauses :test-not #'eq :key #'first))))
+    `(cond
+       ((declarationp)
+        ,@(only :header))
+       (t ,@(only :source)))))
+
+(defmacro c/function ((name &key
+                            (staticp nil)
+                            (format :linear)
+                            (ret "void")
+                            (parameters '("void")))
                       &body body)
   (with-gensyms (scope)
     (once-only (staticp ret parameters)
@@ -100,38 +144,49 @@
                    ,name
                    ,parameters)
            (when (eq *c-environment* :declaration)
-             (write-line ";")
-             (return-from ,scope))
+             (princ ";")
+             (pprint-exit-if-list-exhausted))
            (nl)
            (c/block ,@body))))))
 
-(dolist (*c-environment* '(:declaration :definition))
-  (with-c
-    (fresh-line)
-    (c/function (:name "test"
-                 :staticp t
-                 :format :linear
-                 :parameters '("int var"))
-      (princ "int ret;")
-      (nl)
-      (c/switch "var"
-        (c/case (0 :blockp t :breakp t))
-        (c/case (1 :blockp nil :breakp t))
-        (c/case (2 :blockp t :breakp nil))
-        (c/case (3 :blockp nil :breakp nil))
-        (c/case (:default :blockp nil :breakp t)
-          (princ "/* NO */")))))
+(defmacro c/struct ((name &key
+                          (staticp nil)
+                          (declaration '(declarationp)))
+                    &body body)
+  `(with-logical-block
+     (format t "~:[~;static ~]struct ~a" ,staticp ,name)
+     (when ,declaration
+       (princ ";")
+       (pprint-exit-if-list-exhausted))
+     (princ " ")
+     (c/block* ,@body)
+     (princ ";")))
 
-  (with-c
-    (fresh-line)
-    (c/function (:name "maybe_do_something"
-                 :ret "int"
-                 :format :linear
-                 :parameters '("int *a"
-                               "int *b"
-                               "char **c"
-                               "float *very_long_name"
-                               "double *very_long_name_twice_as_long"))
-      (princ "return 0;"))))
-
-
+(defun test ()
+  (dolist (*c-environment* '(:declaration :definition))
+    (with-c
+      (fresh-line)
+      (c/function ("test"
+                   :staticp t
+                   :format :linear
+                   :parameters '("int var"))
+        (princ "int ret;")
+        (nl)
+        (c/switch "var"
+          (c/case (0 :blockp t :breakp t))
+          (c/case (1 :blockp nil :breakp t))
+          (c/case (2 :blockp t :breakp nil))
+          (c/case (3 :blockp nil :breakp nil))
+          (c/case (:default :blockp nil :breakp t)
+            (princ "/* NO */")))))
+    (with-c
+      (fresh-line)
+      (c/function ("maybe_do_something"
+                   :ret "int"
+                   :format :linear
+                   :parameters '("int *a"
+                                 "int *b"
+                                 "char **c"
+                                 "float *very_long_name"
+                                 "double *very_long_name_twice_as_long"))
+        (princ "return 0;")))))
